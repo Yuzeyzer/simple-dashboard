636a3e2ec6cb2d376c0d9d2867da7e13d07cccda
Yuzeyzer <kurbanof.atabek@gmail.com>
2020-12-16 23:45:21 +0600

    add func that takes data from form and renders it in table (Axios + reactHooks)

--- src/App.js
+++ src/App.js
@@ -2,21 +2,13 @@ import React from 'react';
 import Header from './components/header';
 import Sidebar from './components/sidebar';
 import Manage from './pages/manage/index';
-import axios from 'axios';
 
 function App() {
-  const [munarbek, setMunarbek] = React.useState([]);
-  React.useEffect(async () => {
-    await axios
-      .get(`http://localhost:3000/database.json`)
-      .then((atati) => atati)
-      .then(({ data }) => setMunarbek(data.users));
-  }, []);
   return (
     <div className='App'>
       <Header />
       <Sidebar />
-      <Manage data={munarbek} />
+      <Manage />
     </div>
   );
 }
--- src/pages/manage/index.js
+++ src/pages/manage/index.js
@@ -2,10 +2,75 @@ import React from 'react';
 import plus from '../../assets/plus-symbol.svg';
 import './style.scss';
 import Item from './item';
+import axios from 'axios';
 
-const Manage = ({ data }) => {
+const Manage = () => {
+  const [usersData, setUsersData] = React.useState([]);
+  const [newUser, setNewUser] = React.useState({
+    id: '',
+    avatar: '',
+    fullName: '',
+    userID:'',
+    email: '',
+    phone: '',
+    dateOfRegistry: ''
+  })
+  const [activeModal, setActiveModal] = React.useState(false);
+
+  const handleClick = () => {
+    setActiveModal(activeModal ? false : true);
+  };
+
+  const atai = new Date()
+
+  const saveData = (event) => {
+    setNewUser({
+      id: '',
+      avatar: event.target.form[1].value,
+      fullName: event.target.form[0].value,
+      userID:event.target.form[2].value,
+      email: event.target.form[4].value,
+      phone: event.target.form[3].value,
+      dateOfRegistry: ''
+    })
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    setUsersData([...usersData,newUser ])
+    setNewUser({
+      id: '',
+      avatar: '',
+      fullName: '',
+      userID:'',
+      email: '',
+      phone: '',
+      dateOfRegistry: ''
+    })
+  }
+
+  React.useEffect(async () => {
+    await axios
+      .get(`http://localhost:3000/database.json`)
+      .then((response) => response)
+      .then(({ data }) => setUsersData(data.users));
+  }, []);
+
   return (
     <section className='manage'>
+      {activeModal && (
+        <div className='modal'>
+          <form onSubmit={(event) => handleSubmit(event)}>
+            <h3>Я есть модалка</h3>
+            <input onChange={(event) => saveData(event)} value={newUser.fullName} type='text' placeholder='Введите Имя Пользователя' />
+            <input onChange={(event) => saveData(event)} value={newUser.avatar} type='text' placeholder='Введите путь к картинке' />
+            <input onChange={(event) => saveData(event)} value={newUser.userID} type='text' placeholder='Введите ID' />
+            <input onChange={(event) => saveData(event)} value={newUser.phone} type='text' placeholder='Введите номер телефона' />
+            <input onChange={(event) => saveData(event)} value={newUser.email} type='text' placeholder='Введите почту пользователя' />
+           <button>Добавить пользователя</button>
+          </form>
+        </div>
+      )}
       <div className='manage__top'>
         <div className='row'>
           <div className='manage__filter'>Filter</div>
@@ -13,7 +78,7 @@ const Manage = ({ data }) => {
             <input type='text' placeholder='Search users by name, id' />
           </label>
         </div>
-        <button className='manage__add-user'>
+        <button onClick={handleClick} className='manage__add-user'>
           <img src={plus} alt='plus' />
           ADD USER
         </button>
@@ -26,7 +91,7 @@ const Manage = ({ data }) => {
           <th className='col-3 manage__headers'>Email Address</th>
           <th className='col-1 manage__headers'>Account Created</th>
         </tr>
-          <Item users={data}/>
+        <Item users={usersData} />
       </table>
     </section>
   );
